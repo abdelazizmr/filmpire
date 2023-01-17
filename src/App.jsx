43306a4fe2c428ec79b04691f064e6b9c81aaa51
abdelaziz mrabet <mrabetabdelaziz@gmail.com>
2023-01-17 19:04:24 +0000

    fixed a style

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
      
 
       
-        <Box sx={{backgroundColor: darkmode && 'black',height:'100vh'}} >
+        <Box sx={{backgroundColor: darkmode && 'black',height:'100%'}} >
           
           <Header />
           
